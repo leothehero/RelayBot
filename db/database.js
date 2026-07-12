73e5d02b56e30a79ce0b935e61e5804b7d3ffa12
Tonatsi <leothehero@hotmail.co.uk>
2026-07-12 21:43:25 +0200

    Un-hardcoded the database directory

--- db/database.js
+++ db/database.js
@@ -4,6 +4,7 @@ const path = require('path');
 const Database = require('better-sqlite3');
 
 // --- Define Paths and Detect Environment ---
+const volumePath = process.env.VOLUME_PATH;
 const volumePath = '/data';
 const dbName = 'database.db';
 let dbPath;
